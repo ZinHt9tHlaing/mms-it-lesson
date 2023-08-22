05bf606495590ddca257f9e98534c53b900c9ebc
ZinHt9tHlaing <phyoearkar267@gmail.com>
2023-08-22 21:33:36 +0630

    i no

---
+++ 3-javascript/js/nt.js
@@ -0,0 +1,14 @@
+const myName = "zin htet hlaing";
+console.log(myName);
+
+let x = 5;
+let y = 10;
+console.log(x + y);
+console.log(x > y);
+
+const arr = ['a', 'b', 'c'];
+console.log(arr);
+
+for(x of arr){
+    console.log(x);
+}
