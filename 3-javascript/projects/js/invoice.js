f5a9b081c0145531b6162fc921ac0697fd2abd57
ZinHt9tHlaing <phyoearkar267@gmail.com>
2023-09-13 23:12:12 +0630

    offcanvas coming

--- 3-javascript/projects/js/invoice.js
+++ 3-javascript/projects/js/invoice.js
@@ -37,6 +37,8 @@ const products = [
   },
 ];
 
+// let rowCount = 1;
+
 // selectors
 const app = document.querySelector("#app");
 const recordForm = app.querySelector("#recordForm");
@@ -45,6 +47,7 @@ const quantityInput = app.querySelector("#quantityInput");
 const recordAddBtn = app.querySelector("#recordAddBtn");
 const records = app.querySelector("#records");
 const totalCost = app.querySelector("#totalCost");
+const printer = app.querySelector("#printer");
 
 // functions
 
@@ -56,28 +59,119 @@ const totalCost = app.querySelector("#totalCost");
 // };
 
 const createRecordRow = (product, quantity) => {
-    const tr = document.createElement("tr");
-    const cost = product.price * quantity;
+  const tr = document.createElement("tr");
+  tr.setAttribute("row-product-id", product.id);
+  tr.classList.add("record-row");
+  const cost = product.price * quantity;
   tr.innerHTML = `
-    <td>1</td>
+    <td class='row-num'></td>
     <td>${product.name}</td>
     <td class="text-end">${product.price}</td>
-    <td class="text-end">${quantity}</td>
-    <td class="text-end row-cost">${cost}</td>
+    <td class="text-end row-quantity-control">
+      <i class='bi bi-dash row-quantity-decrement'></i>
+      <span class='row-quantity'>
+      ${quantity}
+      </span>
+      <i class='bi bi-plus row-quantity-increment'></i>
+    </td>
+    <td class="text-end row-control">
+      <span class='row-cost'>${cost}</span>
+      <button class='btn btn-sm btn-primary row-delete'>
+        <i class='bi bi-trash3'></i>
+      </button>
+    </td>
     `;
-    return tr;
+
+  const deleteRow = () => {
+    if (confirm("Are you sure to delete ?")) {
+      tr.remove();
+      costTotal();
+    }
+  };
+
+  const rowDelete = tr.querySelector(".row-delete");
+  rowDelete.addEventListener("click", deleteRow);
+
+  const rowQuantityIncrement = tr.querySelector(".row-quantity-increment");
+  rowQuantityIncrement.addEventListener("click", () => {
+    updateExistedRecord(product, 1);
+  });
+
+  const rowQuantityDecrement = tr.querySelector(".row-quantity-decrement");
+  rowQuantityDecrement.addEventListener("click", () => {
+    updateExistedRecord(product, -1);
+  });
+
+  return tr;
 };
 
 const costTotal = () => {
-    let total = 0;
-    const rowCosts = document.querySelectorAll(".row-cost");
-    rowCosts.forEach(rowCost => {
-        total += parseFloat(rowCost.innerText);
-    });
-    // console.log(total);
-    totalCost.innerText = total;
-    return total;
-}
+  // let total = 0;
+  // const rowCosts = document.querySelectorAll(".row-cost");
+  // console.log([...rowCosts]);
+  // rowCosts.forEach(rowCost => {
+  //     total += parseFloat(rowCost.innerText);
+  // });
+  // console.log(total);
+  totalCost.innerText = [...document.querySelectorAll(".row-cost")].reduce(
+    (pv, cv) => pv + parseFloat(cv.innerText),
+    0
+  );
+  // return total;
+};
+
+const addNewRecord = (product, quantity) => {
+  records.append(createRecordRow(product, quantity));
+};
+
+const updateExistedRecord = (product, quantity) => {
+  const row = document.querySelector(`[row-product-id='${product.id}']`);
+
+  const currentRowQuantity = row.querySelector(".row-quantity");
+  const currentRowCost = row.querySelector(".row-cost");
+
+  if (quantity < 0) {
+    if (currentRowQuantity.innerText > 1) {
+      currentRowQuantity.innerText =
+        parseFloat(currentRowQuantity.innerText) + parseFloat(quantity);
+
+      currentRowCost.innerText = currentRowQuantity.innerText * product.price;
+    }
+  } else {
+    currentRowQuantity.innerText =
+      parseFloat(currentRowQuantity.innerText) + parseFloat(quantity);
+
+    currentRowCost.innerText = currentRowQuantity.innerText * product.price;
+  }
+};
+
+const handleRecordForm = (event) => {
+  event.preventDefault();
+  // console.log("form submit");
+  const data = new FormData(recordForm);
+  // console.log(data.get("productSelect"));
+  // console.log(data.get("quantityInput"));
+
+  const currentProduct = products.find(
+    (product) => product.id == data.get("productSelect")
+  );
+
+  const isExistedProduct = document.querySelector(
+    `[row-product-id='${currentProduct.id}']`
+  );
+
+  // console.log(isExistedProduct);
+  if (isExistedProduct) {
+    updateExistedRecord(currentProduct, data.get("quantityInput"));
+  } else {
+    addNewRecord(currentProduct, data.get("quantityInput"));
+  }
+
+  recordForm.reset();
+
+  // calculate cost
+  costTotal();
+};
 
 // process
 products.forEach((product) => {
@@ -85,21 +179,8 @@ products.forEach((product) => {
   productSelect.append(new Option(product.name, product.id));
 });
 
-recordForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  console.log("form submit");
-  const data = new FormData(recordForm);
-  console.log(data.get("productSelect"));
-  console.log(data.get("quantityInput"));
+recordForm.addEventListener("submit", handleRecordForm);
 
-  const currentProduct = products.find(
-    (product) => product.id == data.get("productSelect")
-    );
-    
-    records.append(createRecordRow(currentProduct, data.get("quantityInput")))
-    
-    recordForm.reset();
-
-    // calculate cost
-    costTotal();
-});
+printer.addEventListener("click",() => {
+  print();
+})
