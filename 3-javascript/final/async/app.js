2411007e7c9963ab69dd201df20b2d14ea3df2a7
ZinHt9tHlaing <phyoearkar267@gmail.com>
2023-11-27 23:42:11 +0630

    async intro

---
+++ 3-javascript/final/async/app.js
@@ -0,0 +1,43 @@
+// fetch("https://fakestoreapi.com/products/1")
+//   .then((res) => res.json())
+//   .then((json) => console.log(json));
+
+// fetch("https://fakestoreapi.com/products/2")
+//   .then((res) => res.json())
+//   .then((json) => console.log(json));
+
+// fetch("https://fakestoreapi.com/products/3")
+//   .then((res) => res.json())
+//   .then((json) => console.log(json));
+
+// fetch("https://fakestoreapi.com/products/4")
+//   .then((res) => res.json())
+//   .then((json) => console.log(json));
+
+// fetch("https://fakestoreapi.com/products/5")
+//   .then((res) => res.json())
+//   .then((json) => console.log(json));
+
+const getData = async () => {
+  const res = await fetch("https://fakestoreapi.com/products/1");
+  const json = await res.json();
+  console.log(json);
+
+  const res2 = await fetch("https://fakestoreapi.com/products/2");
+  const json2 = await res2.json();
+  console.log(json2);
+
+  const res3 = await fetch("https://fakestoreapi.com/products/3");
+  const json3 = await res3.json();
+  console.log(json3);
+
+  const res4 = await fetch("https://fakestoreapi.com/products/4");
+  const json4 = await res4.json();
+  console.log(json4);
+
+  const res5 = await fetch("https://fakestoreapi.com/products/5");
+  const json5 = await res5.json();
+  console.log(json5);
+};
+
+getData();
