a1dfc67994b53ec0eb958bef3b3ea5b385844e48
ZinHt9tHlaing <phyoearkar267@gmail.com>
2023-12-05 11:48:41 +0630

    vite-crud

--- 6-vite/vite-crud/main.js
+++ 6-vite/vite-crud/main.js
@@ -1,2 +1,6 @@
 import './style.css'
 import './node_modules/flowbite/dist/flowbite'
+import School from './src/School'
+
+const school = new School();
+school.init();
---
+++ 6-vite/vite-crud/src/School.js
@@ -0,0 +1,12 @@
+import listeners from "./core/listeners";
+import renders from "./core/renders";
+
+class School {
+    init() {
+        console.log("School Start");
+        renders();
+        listeners();
+    }    
+}
+
+export default School;
