41a6493baf59df73f21af031f142b93fac699db9
ZinHt9tHlaing <phyoearkar267@gmail.com>
2024-01-19 21:27:14 +0630

    typescript day-1

---
+++ 9-typescript/ts-day1/index.ts
@@ -0,0 +1,36 @@
+const sum = (x: number, y: number):void => {
+  //   return x + y;
+  console.log(1 + 2);
+};
+
+sum(1, 2);
+// console.log(sum(1, 2));
+
+// object
+// interface personDT {
+//   name: string;
+//   age: number;
+//   gender: string;
+//   gf: boolean;
+//   hello?: string;
+// }
+
+// type nrc = {
+//   nation: string;
+// };
+
+// let person: personDT & nrc = {
+//   name: "Christ",
+//   age: 21,
+//   gender: "male",
+//   gf: false,
+//   nation: "Myanmar",
+// };
+
+// console.table(person);
+
+// let male: boolean = true;
+// let num: number = 1;
+// let str: string = "Christ";
+
+// let num2: number;
